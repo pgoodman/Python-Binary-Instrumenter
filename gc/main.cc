ff286240d7bf4ec88b1fcdeda1c92076b6f096de
pgoodman <peter.goodman@gmail.com>
2013-01-25 14:54:06 -0500

    Made it so that the GC runs as the main thread and the program's main function runs as a separate thread.

--- gc/main.cc
+++ gc/main.cc
@@ -1,9 +1,58 @@
 
+#include <atomic>
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 extern int program_main(int, char **);
 
+namespace {
+
+    static std::atomic<bool> MAIN_THREAD_DONE(ATOMIC_VAR_INIT(false));
+
+    struct main_thread_args {
+        int argc;
+        char **argv;
+        int return_value;
+    };
+
+    /// The main thread of the program.
+    static void *main_thread_func(main_thread_args *args) {
+        args->return_value = program_main(
+            args->argc,
+            args->argv);
+
+        MAIN_THREAD_DONE.store(true);
+
+        return nullptr;
+    }
+}
+
 int main(int argc, char **argv) throw() {
-    // TODO: setup GC state
-    int ret(program_main(argc, argv));
+
+    typedef void *(pthread_func_t)(void *);
+
+    main_thread_args args = {
+        argc, argv, 0
+    };
+
+    pthread_t main_thread = pthread_t();
+    pthread_attr_t main_thread_attr;
+
+    pthread_attr_init(&main_thread_attr);
+    pthread_create(
+        &main_thread,
+        &main_thread_attr,
+        (pthread_func_t *) main_thread_func,
+        &args);
+
+    for(; !MAIN_THREAD_DONE.load(); ) {
+        usleep(1000);
+
+        // TODO: do a concurrent leak check!
+    }
+
     // TODO: tear down GC state
-    return ret;
+    return args.return_value;
 }
