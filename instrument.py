1fab6e2eb8caabdea497a1acf807e76a921626be
pgoodman <peter.goodman@gmail.com>
2013-01-25 14:21:07 -0500

    tabs to spaces

--- instrument.py
+++ instrument.py
@@ -5,14 +5,14 @@ import pprint
 # its index. Note: the last instruction in a basic block is not
 # necessarily the last item in the instr_list.
 def get_last_instruction(instr_list):
-	for i, line in enumerate(reversed(instr_list)):
-		if line.startswith(".") or line.startswith("_"):
-			continue
-		return len(instr_list) - i, line
+    for i, line in enumerate(reversed(instr_list)):
+        if line.startswith(".") or line.startswith("_"):
+            continue
+        return len(instr_list) - i, line
 
 def visit_basic_block(instr_list):
-	pprint.pprint(instr_list)
-	print get_last_instruction(instr_list)
-	print
+    pprint.pprint(instr_list)
+    print get_last_instruction(instr_list)
+    print
 
-	return instr_list
+    return instr_list
